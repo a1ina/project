6036c76c35f41c6019504261439b74cc0db0b083
a1ina <alina.ma1ina@mail.ru>
2016-09-28 01:27:11 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Header
+ *
+ * @package /*name*/
+ * @since /*name*/
+ *
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="theme-color" content="#3e3e3e">
+	<title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63759233-1', 'auto');
+  ga('send', 'pageview');
+  </script>
+</head>
+<body <?php body_class(); ?>>
+  <header class="header"></header>
