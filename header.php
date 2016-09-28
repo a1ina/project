e7ee0e29a48261e648ec35e6d1d619b60096a87d
a1ina <alina.ma1ina@mail.ru>
2016-09-28 17:37:03 +0300

    Update header.php

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
   <meta name="description" content="">
   <meta name="theme-color" content="#3e3e3e">
 	<title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
+	<title><?php bloginfo('name'); ?><?php wp_title( ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
